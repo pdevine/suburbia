3c5505aa3449434753ed06b0b39073371b5634be
Patrick Devine <pdevine@sonic.net>
2009-05-02 00:15:55 -0700

    tilting garbage

--- gamelib/garbage.py
+++ gamelib/garbage.py
@@ -22,29 +22,25 @@ class GarbageCan:
         self.can_active = False
         self.fallen = False
 
-        self.can_rect = Rect(100, 100, self.image_closed)
+        self.can_rect = Rect(0, 0, self.image_closed)
+        self.can_rect.center = (100, 100)
+
+        self.can_sprite = Sprite(self.image_opened)
+        self.can_sprite.set_position(100 - self.image_opened.width / 2,
+                                     100 - self.image_opened.height / 2)
+
         self.lid_rect = Rect(200, 200, self.image_lid)
 
         window.game_window.push_handlers(self.on_mouse_release)
         window.game_window.push_handlers(self.on_mouse_press)
         window.game_window.push_handlers(self.on_mouse_drag)
 
-        self.sprite = Sprite(self.image_opened)
         events.AddListener(self)
 
     def draw(self):
-        if not self.can_active:
-            if self.opened:
-                if self.fallen:
-                    self.sprite.draw()
-                else:
-                    self.image_opened.blit(*self.can_rect.bottomleft)
-                
-            else:
-                self.image_closed.blit(*self.can_rect.bottomleft)
-        if not self.lid_active and self.opened:
+        self.can_sprite.draw()
+        if self.opened:
             self.image_lid.blit(*self.lid_rect.bottomleft)
-
 
     def on_mouse_press(self, x, y, button, modifiers):
         if button == 1:
@@ -53,33 +49,29 @@ class GarbageCan:
             elif self.can_rect.collide_point(x, y):
                 self.fallen = False
                 self.set_can_active()
+                self.can_sprite.rotation = 0
+
+                if self.opened:
+                    self.can_sprite.image = self.image_opened
+                else:
+                    self.can_sprite.image = self.image_closed
 
     def set_can_active(self):
-        if self.opened:
-            image = self.image_opened
-        else:
-            image = self.image_closed
-
-        cursor = pyglet.window.ImageMouseCursor(
-            image, image.width/2, image.height/2)
-
-        window.game_window.set_mouse_cursor(cursor)
+        window.game_window.set_mouse_visible(False)
         self.can_active = True
         events.Fire('CanTaken')
 
     def set_lid_active(self):
-        cursor = pyglet.window.ImageMouseCursor(
-            self.image_lid,
-            self.image_lid.width/2,
-            self.image_lid.height/2)
-        window.game_window.set_mouse_cursor(cursor)
+        window.game_window.set_mouse_visible(False)
         self.lid_active = True
         events.Fire('LidTaken')
 
     def on_mouse_release(self, x, y, button, modifiers):
+        window.game_window.set_mouse_visible(True)
         #print "x=%d y=%d" % (x, y)
         if self.lid_active:
             if self.can_rect.collide_point(x, y):
+                self.can_sprite.image = self.image_closed
                 self.opened = False
                 events.Fire('LidClosed')
             else:
@@ -91,8 +83,10 @@ class GarbageCan:
             self.set_default_cursor()
 
         elif self.can_active:
-            self.can_rect.x = x
-            self.can_rect.y = y
+            self.can_sprite.rotation = 0
+            self.can_rect.center = (x, y)
+            self.can_sprite.set_position(x-self.image_opened.width/2,
+                                         y-self.image_opened.height/2)
 
             self.set_default_cursor()
             self.can_active = False
@@ -100,27 +94,33 @@ class GarbageCan:
 
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
         if self.can_active:
+            self.can_rect.x = x
+            self.can_rect.y = y
+            self.can_sprite.set_position(x, y)
+
             dist = math.sqrt(abs(dx*dx + dy*dy))
+            self.can_sprite.rotation = dx / 10.0 * 45
+
             if dist > 10:
                 self.fallen = True
                 self.can_active = False
-                self.can_rect.x = x
-                self.can_rect.y = y
-                self.sprite.set_position(x, y)
                 if dx > 0:
-                    self.sprite.rotation = 90
+                    self.can_sprite.rotation = 90
                 else:
-                    self.sprite.rotation = -90
+                    self.can_sprite.rotation = -90
 
                 events.Fire('CanTipped')
 
                 if not self.opened:
                     self.opened = True
+                    self.can_sprite.image = self.image_opened
                     self.lid_rect.bottomleft = self.can_rect.bottomleft
                     self.lid_rect.x += dx * 5
                     self.lid_rect.y += dy * 5
 
                 self.set_default_cursor()
+        elif self.lid_active:
+            self.lid_rect.center = (x, y)
 
     def set_default_cursor(self):
         cursor = window.game_window.get_system_mouse_cursor(
